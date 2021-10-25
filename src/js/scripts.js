458c33edc0f461b4ab3d5b5d01fd259428aca88c
Craig McNaughton <therealcraigmcn@gmail.com>
2021-10-25 10:34:10 -0700

    Add eslint, update settings
    - run fixes

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,13 +2,13 @@ import Bouncer from 'formbouncerjs'
 import { initAlertClose } from './alerts'
 import { initCollapse } from './collapse'
 
-window.addEventListener('load', function() {
-  const validate = new Bouncer('form', {
-    messageAfterField: false,
-    fieldClass: 'form__control--hasError',
-    errorClass: 'form__control-error',
-  })
+window.addEventListener('load', function () {
+    const validate = new Bouncer('form', { // eslint-disable-line
+        messageAfterField: false,
+        fieldClass: 'form__control--hasError',
+        errorClass: 'form__control-error',
+    })
 
-  initAlertClose()
-  initCollapse()
+    initAlertClose()
+    initCollapse()
 })

e0191ced10f4e1a9f7eee1df0765e0c308117e92
Craig McNaughton <therealcraigmcn@gmail.com>
2026-06-15 14:56:14 -0700

    chore: set dev server port to 3010
    Change BrowserSync port from 3020 to 3010 to avoid a conflict with
    the `files` repo which also runs on 3020. All active projects now
    have unique dev server ports.

--- gulpfile.js
+++ gulpfile.js
@@ -165,7 +165,7 @@ gulp.task('reload', (done) => {
 // Browser sync
 gulp.task('browserSync', () => {
   browserSync.init({
-    port: 3020,
+    port: 3010,
     server: './tmp',
     ui: false,
   });
